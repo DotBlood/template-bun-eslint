3619b99e8e5b4d3c5ca131171f96231f37b1f2c0
DotBlood <Tedrip@yandex.ru>
2023-09-24 19:50:54 +0300

    refactor

--- internal/config/config.ts
+++
@@ -1,8 +0,0 @@
-import configFile from '@cfg/config.json'
-
-type TConfig = typeof configFile
-
-export default function getConfig (): TConfig {
-  if (configFile === undefined && configFile === null && typeof configFile === 'undefined') throw new Error('error not soche  cfg file')
-  return configFile
-}
